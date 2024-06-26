2409f1575a5a5258f77bcb307b8166e771390c4d
MURAKAMI Masahiko <fossamagna2@gmail.com>
2024-06-26 12:46:29 +0900

    feat: open AWS console for `AWS::AppSync::Resolver` and `Custom::AmplifyDynamoDBTable`

---
+++ src/console-url-builder/index.test.ts
@@ -0,0 +1,46 @@
+import { test, describe } from "mocha";
+import * as assert from "node:assert";
+import { buildUrl } from ".";
+
+describe("Console URL Builder Test Suite", () => {
+  test("AWS::CloudFormation::Stack", () => {
+    const url = buildUrl({
+      ResourceType: "AWS::CloudFormation::Stack",
+      StackId:
+        "arn:aws:cloudformation:ap-northeast-1:123456789012:stack/myteststack/abc",
+      PhysicalResourceId:
+        "arn:aws:cloudformation:ap-northeast-1:123456789012:stack/amplify-amplifyvitereacttemplate-fossamagna-sandbox-5f9286339c-data123456-Q4HYM3CGZQQQ/b32ba610-2941-11ef-9062-061f906d3db3",
+    });
+    assert.strictEqual(
+      url,
+      "https://ap-northeast-1.console.aws.amazon.com/cloudformation/home?region=ap-northeast-1#/stacks/stackinfo?stackId=arn%3Aaws%3Acloudformation%3Aap-northeast-1%3A123456789012%3Astack%2Famplify-amplifyvitereacttemplate-fossamagna-sandbox-5f9286339c-data123456-Q4HYM3CGZQQQ%2Fb32ba610-2941-11ef-9062-061f906d3db3"
+    );
+  });
+
+  test("AWS::AppSync::Resolver", () => {
+    const url = buildUrl({
+      ResourceType: "AWS::AppSync::Resolver",
+      StackId:
+        "arn:aws:cloudformation:ap-northeast-1:123456789012:stack/myteststack/abc",
+      PhysicalResourceId:
+        "arn:aws:appsync:ap-northeast-1:123456789012:apis/abcdefghijklmnopqrstuvwxyz/types/Mutation/resolvers/updateTodo",
+    });
+    assert.strictEqual(
+      url,
+      "https://ap-northeast-1.console.aws.amazon.com/appsync/home?region=ap-northeast-1#/abcdefghijklmnopqrstuvwxyz/v1/schema/Mutation/updateTodo/resolver"
+    );
+  });
+
+  test("Custom::AmplifyDynamoDBTable", () => {
+    const url = buildUrl({
+      ResourceType: "Custom::AmplifyDynamoDBTable",
+      StackId:
+        "arn:aws:cloudformation:ap-northeast-1:123456789012:stack/myteststack/abc",
+      PhysicalResourceId: "Todo-1234567890-NOE",
+    });
+    assert.strictEqual(
+      url,
+      "https://ap-northeast-1.console.aws.amazon.com/dynamodbv2/home?region=ap-northeast-1#table?name=Todo-1234567890-NOE&tab=overview"
+    );
+  });
+});
--- src/console-url-builder/index.ts
+++ src/console-url-builder/index.ts
@@ -1,66 +1,75 @@
-import { StackResource } from "@aws-sdk/client-cloudformation";
+import type { StackResource } from "@aws-sdk/client-cloudformation";
 
-export function buildUrl(stackResource: StackResource) {
+export function buildUrl(
+  stackResource: Pick<
+    StackResource,
+    "ResourceType" | "PhysicalResourceId" | "StackId"
+  >
+) {
   if (!stackResource.ResourceType) {
     return;
   }
   const urlBuilder = urlBuilders[stackResource.ResourceType];
   if (urlBuilder) {
-    return urlBuilder(stackResource);
+    const region = stackResource.StackId?.split(":")[3]!;
+    return urlBuilder(stackResource.PhysicalResourceId!, region);
   }
 }
 
-const urlBuilders: Record<string, (stackResource: StackResource) => string> = {
-  "AWS::CloudFormation::Stack": (stackResource) => {
-    const region = stackResource.StackId?.split(":")[3];
-    const resourceId = stackResource.PhysicalResourceId!;
+const urlBuilders: Record<
+  string,
+  (physicalResourceId: string, region: string) => string
+> = {
+  "AWS::CloudFormation::Stack": (physicalResourceId, region) => {
+    const resourceId = physicalResourceId!;
     return `https://${region}.console.aws.amazon.com/cloudformation/home?region=${region}#/stacks/stackinfo?stackId=${encodeURIComponent(
       resourceId
     )}`;
   },
-  "AWS::Cognito::IdentityPool": (stackResource) => {
-    const region = stackResource.StackId?.split(":")[3];
-    const resourceId = stackResource.PhysicalResourceId;
+  "AWS::Cognito::IdentityPool": (physicalResourceId, region) => {
+    const resourceId = physicalResourceId;
     return `https://${region}.console.aws.amazon.com/cognito/v2/identity/identity-pools/${resourceId}/user-statistics?region=${region}`;
   },
-  "AWS::Cognito::UserPool": (stackResource) => {
-    const region = stackResource.StackId?.split(":")[3];
-    const resourceId = stackResource.PhysicalResourceId;
+  "AWS::Cognito::UserPool": (physicalResourceId, region) => {
+    const resourceId = physicalResourceId;
     return `https://${region}.console.aws.amazon.com/cognito/v2/idp/user-pools/${resourceId}/users?region=${region}`;
   },
-  "AWS::AppSync::GraphQLApi": (stackResource) => {
-    const region = stackResource.StackId?.split(":")[3];
-    const resourceId = stackResource.PhysicalResourceId?.split("/", 2)[1];
+  "AWS::AppSync::GraphQLApi": (physicalResourceId, region) => {
+    const resourceId = physicalResourceId?.split("/", 2)[1];
     return `https://${region}.console.aws.amazon.com/appsync/home?region=${region}#/${resourceId}/v1/home`;
   },
-  "AWS::AppSync::GraphQLSchema": (stackResource) => {
-    const region = stackResource.StackId?.split(":")[3];
-    const resourceId = stackResource.PhysicalResourceId?.split("/", 2)[1];
+  "AWS::AppSync::GraphQLSchema": (physicalResourceId, region) => {
+    const resourceId = physicalResourceId.split("/", 2)[1];
     return `https://${region}.console.aws.amazon.com/appsync/home?region=${region}#/${resourceId}/v1/schema`;
   },
-  "AWS::AppSync::DataSource": (stackResource) => {
-    const region = stackResource.StackId?.split(":")[3];
-    const resourceId = stackResource.PhysicalResourceId?.split("/", 2)[1];
+  "AWS::AppSync::DataSource": (physicalResourceId, region) => {
+    const resourceId = physicalResourceId?.split("/", 2)[1];
     return `https://${region}.console.aws.amazon.com/appsync/home?region=${region}#/${resourceId}/v1/datasources`;
   },
-  "AWS::Lambda::Function": (stackResource) => {
-    const region = stackResource.StackId?.split(":")[3];
-    const resourceId = stackResource.PhysicalResourceId;
+  "AWS::AppSync::Resolver": (physicalResourceId, region) => {
+    const apiId = physicalResourceId.split("/")[1];
+    const resolver = physicalResourceId.split("/")[3];
+    const operation = physicalResourceId.split("/")[5];
+    return `https://${region}.console.aws.amazon.com/appsync/home?region=${region}#/${apiId}/v1/schema/${resolver}/${operation}/resolver`;
+  },
+  "AWS::Lambda::Function": (physicalResourceId, region) => {
+    const resourceId = physicalResourceId;
     return `https://${region}.console.aws.amazon.com/lambda/home?region=${region}#/functions/${resourceId}`;
   },
-  "AWS::S3::Bucket": (stackResource) => {
-    const region = stackResource.StackId?.split(":")[3];
-    const resourceId = stackResource.PhysicalResourceId;
+  "AWS::S3::Bucket": (physicalResourceId, region) => {
+    const resourceId = physicalResourceId;
     return `https://${region}.console.aws.amazon.com/s3/buckets/${resourceId}?region=${region}`;
   },
-  "AWS::IAM::Role": (stackResource) => {
-    const region = stackResource.StackId?.split(":")[3];
-    const resourceId = stackResource.PhysicalResourceId;
+  "AWS::IAM::Role": (physicalResourceId, region) => {
+    const resourceId = physicalResourceId;
     return `https://${region}.console.aws.amazon.com/iam/home?region=${region}#/roles/${resourceId}`;
   },
-  "AWS::IAM::Policy": (stackResource) => {
-    const region = stackResource.StackId?.split(":")[3];
-    const resourceId = stackResource.PhysicalResourceId;
+  "AWS::IAM::Policy": (physicalResourceId, region) => {
+    const resourceId = physicalResourceId;
     return `https://${region}.console.aws.amazon.com/iam/home?region=${region}#/policies/${resourceId}`;
+  },
+  "Custom::AmplifyDynamoDBTable": (physicalResourceId, region) => {
+    const tableName = physicalResourceId;
+    return `https://${region}.console.aws.amazon.com/dynamodbv2/home?region=${region}#table?name=${tableName}&tab=overview`;
   },
 };
