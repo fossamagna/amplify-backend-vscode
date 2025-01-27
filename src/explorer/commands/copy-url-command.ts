849ff6bdf46a22755b8dc7ef225114e6994fbdc3
MURAKAMI Masahiko <fossamagna2@gmail.com>
2025-01-27 09:11:47 +0900

    fix: avoid duplication URL encoding when copy URL to clipboard

--- src/explorer/commands/copy-url-command.ts
+++ src/explorer/commands/copy-url-command.ts
@@ -2,6 +2,6 @@ import * as vscode from "vscode";
 import { createUrlCommand } from "./url-command";
 
 export const copyUrlCommand = createUrlCommand(async (uri: vscode.Uri) => {
-  await vscode.env.clipboard.writeText(uri.toString());
+  await vscode.env.clipboard.writeText(uri.toString(true));
   vscode.window.showInformationMessage("URL copied to clipboard");
 });
